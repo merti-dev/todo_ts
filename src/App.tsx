7fc29730633a1775c4416d80cb8e3cfc336354a4
ali mert inal <mert.inal@gmail.com>
2025-04-07 12:02:01 +0200

    given type of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { useState , FC} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const  App:FC = () => {
   const [count, setCount] = useState(0)
 
   return (
